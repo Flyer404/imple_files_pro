95084bd25ea6f2633210c8edf081fcb6aa7a9c6c
Flyer404 <timcook20212022@gmail.com>
2022-10-23 01:40:20 +0200

    prime number check

--- Js_file_1.js
+++ Js_file_1.js
@@ -1,2 +1,8 @@
 // this is a javascript file 
 console.log("Hello world")
+
+// primne check
+const isPrime = num => {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false; 
+    return num > 1;
